f7dd5754c310c0e8d8126aaf6b09348b3da3a9b8
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-08-24 08:54:50 +0300

    02-Singleton Design Pattern - Asp.NET Core

---
+++ 02-Singleton Design Pattern - Asp.NET Core/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using _02_Singleton_Design_Pattern___Asp.NET_Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_Singleton_Design_Pattern___Asp.NET_Core.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet("x")]
+    public IActionResult X()
+    {
+        var dataBase = DataBaseService.Instance;
+        dataBase.Connection();
+        dataBase.DisConnection();
+        return Ok(dataBase.Count);
+    }
+    [HttpGet("y")]
+    public IActionResult Y()
+    {
+        var dataBase = DataBaseService.Instance;
+        dataBase.Connection();
+        dataBase.DisConnection();
+        return Ok(dataBase.Count);
+    }
+}
---
+++ 02-Singleton Design Pattern - Asp.NET Core/Services/DataBaseService.cs
@@ -0,0 +1,33 @@
+﻿namespace _02_Singleton_Design_Pattern___Asp.NET_Core.Services;
+
+public class DataBaseService
+{
+    private DataBaseService()
+    {
+        Console.WriteLine($"{nameof(DataBaseService)} is created.");
+    }
+
+    private static DataBaseService _dataBaseService;
+    public static DataBaseService Instance
+    {
+        get
+        {
+            if (_dataBaseService == null)
+                _dataBaseService = new DataBaseService();
+            return _dataBaseService;
+        }
+    }
+    public int Count { get; set; }
+
+    public bool Connection()
+    {
+        Count++;
+        Console.WriteLine("Bağlantı sağlandı ...");
+        return true;
+    }
+    public bool DisConnection()
+    {
+        Console.WriteLine("Bağlantı koparıldı ...");
+        return false;
+    }
+}
