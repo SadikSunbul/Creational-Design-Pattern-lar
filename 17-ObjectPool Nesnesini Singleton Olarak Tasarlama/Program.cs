d4fd6812dff64d7d2113d34bed9093712a9d3e3e
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-08-27 17:29:13 +0300

    Object Pool Design Pattern #4 - Asenkron Süreçlerde Örneklendirme

---
+++ 17-ObjectPool Nesnesini Singleton Olarak Tasarlama/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Channels;
+
+ObjectPool<X> pools = ObjectPool<X>.GetInstance; //Singleton pool olmalıdır heryerden aynı havuza ulasmak lazım değilse poola eleman eklemenın bır manası kalmaz
+var x = pools.Get(() => new X());//Havuzda x türünden bir nesne var ise sen bana onu getir yok ise x i üret bana gönder der
+x.Count++;
+pools.Return(x); //buradaki x nesnesini havuza atıyoruz
+
+var x1 = pools.Get(() => new X()); //burada x havuzdan gelicektir 
+x1.Count++;
+pools.Return(x1);//bunu gene yazmalıyızki sonradan birdaha kullanmak istersek havuzda bulunsun atmaz isek artık x nesnesi kaybolucaktır 
+
+var x2 = pools.Get(() => new X()); //burada x havuzdan gelicektir 
+x2.Count++;
+pools.Return(x2);//bunu gene yazmalıyızki sonradan birdaha kullanmak istersek havuzda bulunsun atmaz isek artık x nesnesi kaybolucaktır 
+
+Console.WriteLine();
+class ObjectPool<T> where T : class //sigleton uygulncak
+{
+    //bize içindeki nesneyi verir verirkende bu koleksıyondan cıkartarak verir işimiz bittiği zaman geri ekler kolaksiyona 
+    readonly ConcurrentBag<T> _instances; //pool
+    private ObjectPool()
+    {
+        _instances = new ConcurrentBag<T>();
+    }
+    static ObjectPool<T> _objectPool;
+    static ObjectPool()
+       => _objectPool = new();
+
+    public static ObjectPool<T> GetInstance { get => _objectPool; }
+
+    public T Get(Func<T>? _objectGenerator = null)
+    {
+        //Havuzdan generik parametrede bıldırılen turdekı nesneyı geri döndürmek.
+
+
+        return _instances.TryTake(out T instance) ? instance : _objectGenerator();
+    }
+
+    public void Return(T instances)
+    {
+        //Havuzdan ödünc alınan nesneyi iade etmek
+
+        _instances.Add(instances);
+    }
+}
+
+class X
+{
+    public int Count { get; set; }
+
+    public void Write()
+        => Console.WriteLine(Count);
+
+    public X()//buralarda malıyetlı bır işlem oldugunu varsayalım
+        => Console.WriteLine("X üretim maliyeti...");
+
+    ~X()//buralarda malıyetlı bır işlem oldugunu varsayalım
+        => Console.WriteLine("X imha maliyeti....");
+}
+
+
