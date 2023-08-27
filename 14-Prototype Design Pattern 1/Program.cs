3ef204d312970ad4e15c4f86496a5d7f9f0e60be
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-08-27 11:43:57 +0300

    Prototype Design Pattern

---
+++ 14-Prototype Design Pattern 1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+Person person1 = new("Sadık", "Sünbül", Department.C, 100, 10);
+//Person person2 = new("Ahmet", "Sünbül", Department.C, 100, 10);
+
+
+Person person2 = person1.Clone();
+person2.Name = "Ahmet";
+
+Console.WriteLine();
+
+//Abstract Prototype
+interface IPersonCloneable
+{
+    Person Clone();
+}
+
+//Concrete Prototype
+class Person : IPersonCloneable
+{
+    public Person(string name, string surname, Department department, int salary, int premium)
+    {
+        Name = name;
+        Surname = surname;
+        Department = department;
+        Salary = salary;
+        Premium = premium;
+        Console.WriteLine("Person nesnesi oluşturuldu.");
+    }
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public Department Department { get; set; }
+    public int Salary { get; set; }
+    public int Premium { get; set; }
+
+    public Person Clone()
+    {
+        return (Person)base.MemberwiseClone();
+    }
+}
+
+enum Department
+{
+    A, B, C
+}
