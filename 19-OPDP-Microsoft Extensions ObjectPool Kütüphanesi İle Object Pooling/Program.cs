606cea12032e66cb2b39850aff18e51d771fe7f3
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-08-28 08:43:08 +0300

    Microsoft.Extensions.ObjectPool

---
+++ 19-OPDP-Microsoft Extensions ObjectPool Kütüphanesi İle Object Pooling/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.ObjectPool;
+
+//Microsoft.Extensions.ObjectPool İnmelidir
+
+
+
+
+DefaultObjectPoolProvider provider = new();
+ObjectPool<X> pool = provider.Create(new DefaultPooledObjectPolicy<X>()); //object pool olusturulmasını basalattık
+
+var x1 = pool.Get();
+x1.Count++;
+x1.Write();
+pool.Return(x1);
+
+var x2 = pool.Get();
+x2.Count++;
+x2.Write();
+pool.Return(x2);
+
+
+class X
+{
+    public int Count { get; set; }
+    public void Write()
+        => Console.WriteLine(Count);
+
+    public X()
+        => Console.WriteLine("X Üretim maliyeti");
+
+    ~X()
+        => Console.WriteLine("X imha maliyeti ....");
+}
